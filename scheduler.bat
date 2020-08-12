27e0e683dad6634c4eab8f47801228fe72262dd1
Ram <ramkumar.yoganathan@outlook.com>
2020-08-12 17:46:54 +0530

    Edited logback xml file.

--- scheduler.bat
+++ scheduler.bat
@@ -3,7 +3,15 @@
 SETLOCAL ENABLEEXTENSIONS
 :: Set title of the window
 TITLE Carpe-Diem AutoBot
+:: Set Maven Home
 SET MAVEN_HOME=C:\apache-maven-3.6.1\bin
-CD C:\development\carpe-diem-autobot
+:: Clean and Create WorkSpace
+RMDIR C:\Runner /F /Q
+MKDIR C:\Runner
+:: Git Clone Repository
+git clone https://00ff8871658427b9ecb10d1385a81e4bffe9569c@github.com/ramkumar-yoganathan/carpe-diem-autobot.git
+CD C:\Runner\carpe-diem-autobot
+COPY C:\development\carpe-diem-autobot\src\main\resources\application.properties C:\Runner\carpe-diem-autobot\src\main\resources\application.properties
+:: Run App
 %MAVEN_HOME%\mvn clean spring-boot:run
 ENDLOCAL
